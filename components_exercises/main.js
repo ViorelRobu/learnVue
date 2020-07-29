cdd53e8addd05a8b8c89ff3951d373407ed71c9c
Viorel Robu <wwjd.vio@gmail.com>
2020-07-29 22:20:00 +0300

    vue components examples part 1

--- components_exercises/main.js
+++ components_exercises/main.js
@@ -9,21 +9,36 @@ Vue.component('message', {
         <article class="message" v-show="isVisible">
             <div class="message-header">
                 {{ title }}
-                <button type="button" @click="hideModal">x<button>
+            <button type="button" @click="this.isVisible = false">x</button>
             </div>
             <div class="message-body">
                 {{ body }}
             </div>
         </article>
-    `,
-    methods: {
-        hideModal() {
-            this.isVisible = false;
-        }
-    }
+    `
+});
+
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <p>
+                        <slot></slot>
+                    </p>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
+    `
 });
 
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        showModal: false
+    }
 });
